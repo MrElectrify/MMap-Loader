5b8ff86b3fbc1c7ac872036477b8669d0fd69881
Andrew Buck <aman560900@hotmail.com>
2020-08-09 11:24:36 -0400

    Initial full

---
+++ src/MMap.cpp
@@ -0,0 +1,15 @@
+#include <BlackBone/ManualMap/MMap.h>
+#include <BlackBone/Process/Process.h>
+
+#include <vector>
+
+#include <Windows.h>
+
+bool Inject(HANDLE hProc, void* buffer, size_t len)
+{
+	blackbone::Process process;
+	if (process.Attach(hProc) != STATUS_SUCCESS)
+		return false;
+	const auto image = process.mmap().MapImage(len, buffer);
+	return image.success();
+}
