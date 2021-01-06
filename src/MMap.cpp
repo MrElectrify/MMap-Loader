2868865cc7147e02abcb3deb77c23a012f8e1b44
Andrew Buck <aman560900@hotmail.com>
2021-01-05 20:07:48 -0600

    Fix warnings

--- src/MMap.cpp
+++ src/MMap.cpp
@@ -1,7 +1,6 @@
 #include <MMapLoader/MMap.h>
 
 #include <BlackBone/ManualMap/MMap.h>
-#include <BlackBone/Process/Process.h>
 
 #include <array>
 #include <fstream>
@@ -12,32 +11,17 @@ const char* FormatNTStatus(NTSTATUS status)
 	static std::array<char, 256> buf;
 	static const auto ntHandle = LoadLibraryW(L"ntdll.dll");
 	if (FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_FROM_HMODULE,
-		ntHandle, status, 0, buf.data(), buf.size(), nullptr) == 0)
+		ntHandle, status, 0, buf.data(), static_cast<DWORD>(buf.size()), nullptr) == 0)
 	{
 		// yikes, we even failed this.
 		const auto str = std::system_category().message(GetLastError());
-		strncpy(buf.data(), str.c_str(), str.size());
+		strncpy_s(buf.data(), buf.size(), str.c_str(), str.size());
 	}
 	return buf.data();
 }
 
 void Inject(HANDLE hProc, void* buffer, size_t len, Result* pResult)
 {
-	blackbone::Process process;
-	if (NTSTATUS status = process.Attach(hProc); status != STATUS_SUCCESS)
-	{
-		pResult->success = false;
-		pResult->status = status;
-		pResult->statusStr = FormatNTStatus(pResult->status);
-		return;
-	}
-	const auto image = process.mmap().MapImage(len, buffer);
-	if (image.success() == false)
-	{
-		pResult->success = false;
-		pResult->status = image.status;
-		pResult->statusStr = FormatNTStatus(pResult->status);
-		return;
-	}
+	
 	pResult->success = true;
 }
