c41a098c47fcffd12a85271d6bebf4ff21538a0a
andrewbuck86 <ajbuck@crimson.ua.edu>
2021-01-14 10:30:53 -0600

    Update visibility of headers, GetRVA

--- include/MMapLoader/PortableExecutable.h
+++ include/MMapLoader/PortableExecutable.h
@@ -49,6 +49,20 @@ namespace MMapLoader
 		/// @return The status code
 		std::optional<std::variant<DWORD, NTSTATUS>>
 			ResolveImports(IMAGE_IMPORT_DESCRIPTOR const* pImportDesc) noexcept;
+		/// @brief Gets a structure at an RVA offset
+		/// @tparam T The type to get
+		/// @param offset The offset to the structure
+		/// @return The structure
+		template<typename T>
+		T* GetRVA(uintptr_t offset)
+		{
+			return reinterpret_cast<T*>(
+				reinterpret_cast<uintptr_t>(m_image.get()) + offset);
+		}
+
+		IMAGE_DOS_HEADER m_dosHeader;
+		IMAGE_NT_HEADERS m_ntHeaders;
+		std::vector<IMAGE_SECTION_HEADER> m_sectionHeaders;
 	private:
 		/// @brief Maps the PE file into memory
 		/// @param path The path to the PE file
@@ -90,26 +104,11 @@ namespace MMapLoader
 		/// @return The protection flags
 		static DWORD SectionFlagsToProtectionFlags(DWORD sectionFlags) noexcept;
 
-		/// @brief Gets a structure at an RVA offset
-		/// @tparam T The type to get
-		/// @param offset The offset to the structure
-		/// @return The structure
-		template<typename T>
-		T* GetRVA(uintptr_t offset)
-		{
-			return reinterpret_cast<T*>(
-				reinterpret_cast<uintptr_t>(m_image.get()) + offset);
-		}
-
 		Detail::NT::_LDR_DATA_TABLE_ENTRY64 m_loaderEntry{};
 		Detail::NT::_LDR_DDAG_NODE64 m_ddagNode{};
 
 		std::wstring m_modPath;
 		std::wstring m_modName;
-
-		IMAGE_DOS_HEADER m_dosHeader;
-		IMAGE_NT_HEADERS m_ntHeaders;
-		std::vector<IMAGE_SECTION_HEADER> m_sectionHeaders;
 
 		std::shared_ptr<void> m_image;
 	};
